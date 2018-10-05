003a7a4233f876896f3a74ff9263c3cdee7840ea
Lucashfonseca123 <32250723+Lucashfonseca123@users.noreply.github.com>
2018-10-05 08:52:51 -0300

    Update Relatorio.php

--- algoritmos/SistemaComercialSimples/Relatorio.php
+++ algoritmos/SistemaComercialSimples/Relatorio.php
@@ -10,8 +10,8 @@
     	{
         echo "<br>Codigo produto : " .$dados['codigo'].'<br>';
         echo "Quantidade produto: " .$dados['quantidade'].'<br>';
-    
     }
+    
     echo "<br>***************************************<br>";
     echo "<br>***** LISTA PRODUTOS DISPONÍVEL NO MERCADO *****<br>";
     $sql = mysqli_query($con,"SELECT codigo, quantidade FROM cliente") or die("Erro");		//Faz uma consulta em clientes para fazer o preço
@@ -32,18 +32,18 @@
               }
       	   }
       	    if($dados['codigo'] == $dados2['codigo'] ){                 // Testa se o dado que o cliente quer existe nos produtos
-        	  if ($dados2['quantidade']<11){		
-                $tot += ((double) $dados2['valor_unit'] * (double) $dados['quantidade']);			
+        	  if ($dados['quantidade']<11){		
+                $tot += ((double) $dados2['valor_unit'] * (double) $dados['quantidade']);		
               }
-              if ($dados2['quantidade']>10 &$dados2['quantidade']<21) {
+              if ($dados['quantidade']>10 &$dados['quantidade']<21) {
                 $desconto = ($dados2['valor_unit']*5)/100;
                 $tot += ((double) $dados2['valor_unit']-$desconto)* (double) $dados['quantidade'];	//Aplica descontos de 5%
               }
-              if ($dados2['quantidade']>20 &$dados2['quantidade']<31) {
+              if ($dados['quantidade']>20 &$dados['quantidade']<31) {
                 $desconto = ($dados2['valor_unit']*10)/100;											//Aplica descontos de 10%
                 $tot += ((double) $dados2['valor_unit']-$desconto)* (double) $dados['quantidade'];
               }
-              if ($dados2['quantidade']>30 &$dados2['quantidade']<41) {
+              if ($dados['quantidade']>30 &$dados['quantidade']<41) {
                 $desconto = ($dados2['valor_unit']*20)/100;											//Aplica descontos de 20%
                 $tot += ((double) $dados2['valor_unit']-$desconto)* (double) $dados['quantidade'];
               } 
@@ -53,7 +53,22 @@
        	$i++;	
        	}
         echo "<br>***************************************<br>";
+        echo "<br>***** PRODUTOS VENDIDOS *****<br>";
+        $sql = mysqli_query($con,"SELECT codigo, quantidade FROM cliente") or die("Erro"); 
+        while($dados=mysqli_fetch_assoc($sql)){                         //$dados recebe todo o conteudo da consulta
+        $sql2 = mysqli_query($con,"SELECT nome, codigo, valor_unit, quantidade FROM produto") or die("Erro"); //Consulta em produto
+        while($dados2=mysqli_fetch_assoc($sql2)) {
+           if($dados['codigo'] == $dados2['codigo'] ){ 
+            echo "<br>Numero da venda ".$dados2['codigo'];
+            echo "<br>Data: ";
+            echo date('d/m/Y H:i:s');
+            echo "<br>Nome: " .$dados2['nome'].'<br>';
+            echo "Quantidade: " .$dados['quantidade'].'<br>';
+           }
+        }                       
+       }
        	echo "<br>Total é: ".$tot." R$";
+        echo "<br><br>***************************************<br>";
 ?>
 <!DOCTYPE html>
 <html>
